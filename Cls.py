3adef33b7c7e552f5962c397ca06e9dadef805d4
PetarDamyanov <Petar.A.Damyanov@gmail.com>
2020-03-25 22:24:34 +0200

    call_player func

--- Cls.py
+++ Cls.py
@@ -5,6 +5,14 @@ card_deck=["7s", "8s", "9s", "10c", "Jd", "Qd","Kd","As",
 "7c", "8d", "9c", "10s", "Jh", "Qc", "Kc", "Ad",
 "7h", "8h", "9h", "10h", "Jc", "Qh", "Ks", "Ah"
 ]
+
+lst_calls=["Clubs", "Diamonds", "Hearts", "Spades","No trumps","All trumps"]
+
+
+def player_call():
+	n=rand(0,len(lst_calls))
+	return lst_calls[n]
+
 card_deck_cp=deepcopy(card_deck)
 
 def create_hand():
@@ -19,6 +27,11 @@ class Player:
 	def __init__(self,name):
 		self.name=name
 		self.lst_hand=[]
+		#get belot 50,100 etc
+		self.announcements=[]
+		#get points after round
+		self.points=0
+
 	def __str__(self):
 		return "{0}->{1}".format(self.name,self.lst_hand)
 	
@@ -27,7 +40,12 @@ class Player:
 	
 	def get_hand(self,lst_hand):
 		self.lst_hand=lst_hand
-
+	def get_announcements(self):
+		#check for belot 30 ,50, 100
+		pass
+	def get_points(self):
+		#get points of player 
+		pass
 class Team:
 	def __init__(self,name,lst_player):
 		self.name=name
--- test_cls.py
+++ test_cls.py
@@ -1,21 +1,14 @@
 import unittest
-from Cls import Player,Team,create_hand
-class test_list_hand(unittest.TestCase):
-	# def test_hand_of_player(self):
-	# 	l=create_hand()
-	# 	l1=create_hand()
-	# 	l2=create_hand()
-	# 	l3=create_hand()
-	# 	print(l)
-	# 	print(l1)
-	# 	print(l2)
-	# 	print(l3)
-	pass
+from Cls import Player,Team,create_hand,player_call
+class test_call(unittest.TestCase):
+	def test_call_of_player(self):
+		s=player_call()
+		print(s)
 class test_player_class(unittest.TestCase):
 	def test_player_class_hand_add(self):
 		player=Player("Gosho")
 		player.get_hand(create_hand())
 		print(player)
-
+		
 if __name__ == '__main__':
 	unittest.main()
