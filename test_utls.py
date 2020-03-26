48a44274fbe0207c5a7f5125a800e4adf9c65bed
Yoanna <yoansiyotova@gmail.com>
2020-03-26 15:52:17 +0200

    Reafactor random_hand

--- test_utls.py
+++ test_utls.py
@@ -2,19 +2,19 @@ import unittest
 from utls import random_call, random_hand, inner_sort, sort
 
 class TestInnerSort(unittest.TestCase):
-	def test_inner_sort_with_empty_list_returns_empty_list(self):
-		list_cards = []
+    def test_inner_sort_with_empty_list_returns_empty_list(self):
+        list_cards = []
 
-		res = inner_sort(list_cards)
+        res = inner_sort(list_cards)
 
-		self.assertEqual(res, [])
+        self.assertEqual(res, [])
 
-	def test_inner_sort_with_list_with_cards_returns_sorted_list_by_suites(self):
-		list_cards = ['7s', '10s', 'Ks', 'Js']
+    def test_inner_sort_with_list_with_cards_returns_sorted_list_by_suites(self):
+        list_cards = ['7s', '10s', 'Ks', 'Js']
 
-		res = inner_sort(list_cards)
+        res = inner_sort(list_cards)
 
-		self.assertEqual(res, ['7s', '10s', '11s', '13s'])
+        self.assertEqual(res, ['7s', '10s', '11s', '13s'])
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
--- utls.py
+++ utls.py
@@ -10,86 +10,86 @@ card_deck = ["7s", "8s", "9s", "10s", "Js", "Qs","Ks","As",
 lst_calls = ["Clubs", "Diamonds", "Hearts", "Spades", "No trumps", "All trumps"]
 
 def random_call():
-	n = rand(0,len(lst_calls)-1)
+    n = rand(0,len(lst_calls)-1)
 
-	return lst_calls[n]
+    return lst_calls[n]
 
-def random_hand():
-	card_deck_cp = deepcopy(card_deck)
-	lst_hand = []
+card_deck_cp = deepcopy(card_deck)
+def random_hand(card_deck_for_the_round):
+    lst_hand = []
 
-	for x in range(0,8):
-		n = rand(0,len(card_deck_cp)-1)
-		lst_hand.append(card_deck_cp[n])
-		card_deck_cp.remove(card_deck_cp[n])
+    for x in range(0,8):
+        n = rand(0,len(card_deck_for_the_round)-1)
+        lst_hand.append(card_deck_for_the_round[n])
+        card_deck_for_the_round.remove(card_deck_for_the_round[n])
 
-	return lst_hand
+    return lst_hand
 
 #sorting by the suits(7 8 9 10 J->11 Q->12 K->13 A->14)
 def inner_sort(lst):
-	if lst == []:
-		return lst
-	else:
-		l = []
-		l2 = []
+    if lst == []:
+        return lst
+    else:
+        l = []
+        l2 = []
 
-		#getting the rank 
-		rank = lst[0][-1:]
+        #getting the rank 
+        rank = lst[0][-1:]
 
-		for x in lst:
-			#take only the suite
-			x = x[:-1]
+        for x in lst:
+            #take only the suite
+            x = x[:-1]
 
-			if x == "J":
-				x = "11"
-			elif x == "Q":
-				x = "12"
-			elif x == "K":
-				x = "13"
-			elif x == "A":
-				x = "14"
+            if x == "J":
+                x = "11"
+            elif x == "Q":
+                x = "12"
+            elif x == "K":
+                x = "13"
+            elif x == "A":
+                x = "14"
 
-			l.append(int(x))
+            l.append(int(x))
 
-		l.sort()
+        l.sort()
 
-	return [str(card)+rank for card in l]
+    return [str(card)+rank for card in l]
 
 def sort(lst):
-	lc=[]
-	ld=[]
-	lh=[]
-	ls=[]
+    lc=[]
+    ld=[]
+    lh=[]
+    ls=[]
 #four lists with each rank
-	for x in lst:
-		if len(x)==2:
-			if x[1]=="c":
-				lc.append(x)
-			elif x[1]=="d":
-				ld.append(x)
-			elif x[1]=="h":
-				lh.append(x)
-			elif x[1]=="s":
-				ls.append(x)
-		if len(x)==3:
-			if x[2]=="c":
-				lc.append(x)
-			elif x[2]=="d":
-				ld.append(x)
-			elif x[2]=="h":
-				lh.append(x)
-			elif x[2]=="s":
-				ls.append(x)
+    for x in lst:
+        if len(x)==2:
+            if x[1]=="c":
+                lc.append(x)
+            elif x[1]=="d":
+                ld.append(x)
+            elif x[1]=="h":
+                lh.append(x)
+            elif x[1]=="s":
+                ls.append(x)
+        if len(x)==3:
+            if x[2]=="c":
+                lc.append(x)
+            elif x[2]=="d":
+                ld.append(x)
+            elif x[2]=="h":
+                lh.append(x)
+            elif x[2]=="s":
+                ls.append(x)
 
-	lc=inner_sort(lc)
-	ld=inner_sort(ld)
-	lh=inner_sort(lh)
-	ls=inner_sort(ls)
+    lc=inner_sort(lc)
+    ld=inner_sort(ld)
+    lh=inner_sort(lh)
+    ls=inner_sort(ls)
 
-	l_end=[]
-	l_end.append(ls)
-	l_end.append(lh)
-	l_end.append(ld)
-	l_end.append(lc)
+    l_end=[]
+    l_end.append(ls)
+    l_end.append(lh)
+    l_end.append(ld)
+    l_end.append(lc)
 
-	return l_end
+    return l_end
