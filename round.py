2b2444c92e16aaa5f0d5f9256eee844f1c388cb2
Yoanna <yoansiyotova@gmail.com>
2020-03-29 15:14:00 +0300

    Add final announcements of each player

--- round.py
+++ round.py
@@ -1,17 +1,19 @@
 from Cls import Player,Team
 from utls import random_hand, sort, create_deck_copy, random_call, announcements
+from copy import deepcopy
 '''
 from belote import get_players
 '''
 
 def team_announcements(first_player, second_player):
+    first_player_cp = deepcopy(first_player)
     for key, value in second_player.items():
-        if key in first_player:
+        if key in first_player_cp:
             for cards in value:
-                first_player[key].append(cards)
+                first_player_cp[key].append(cards)
         else:
-            first_player[key] = value
-    return first_player
+            first_player_cp[key] = value
+    return first_player_cp
 
 def compare_team_announcements(first_team, second_team):
     if len(first_team) == 0 and len(second_team) == 0:
@@ -104,6 +106,17 @@ def compare_team_announcements(first_team, second_team):
 
         return[first_team, second_team]
 
+def final_announcements(announce_player, team_announcements):
+    if len(announce_player) == 0 and len(team_announcements) == 0:
+        return []
+    else:
+        for announce, value in team_announcements.items():
+            if announce in announce_player:
+                for a in announce_player[announce]:
+                    if a not in value:
+                        announce_player[announce].remove(a)
+        return list(key for key in announce_player.keys() if len(announce_player[key]) > 0)
+
 def round():
     # call = random_call()
     call = 'All trumps'
@@ -116,52 +129,61 @@ def round():
     
     print('rank: ', call)
     #Player1(first team)
-    first_team.lst_players[0].cards =random_hand(card_deck) 
+    # first_team.lst_players[0].cards =random_hand(card_deck) 
+    first_team.lst_players[0].cards = ['Ad', 'Qd', 'Kc', 'Jc', '7h', '9c', 'Kh', '8d']
     print('Cards Player1: ', first_team.lst_players[0].cards)
 
     print('announcements:', announcements(sort(first_team.lst_players[0].cards), call))
     announcements_first_player = announcements(sort(first_team.lst_players[0].cards), call)
    
-    first_team.lst_players[0].announcements = list(announcements_first_player.keys())
-    print(first_team.lst_players[0].announcements)
-
     #Player2(second team)
-    second_team.lst_players[0].cards =random_hand(card_deck)
+    # second_team.lst_players[0].cards =random_hand(card_deck)
+    second_team.lst_players[0].cards = ['9d', 'Ac', 'Jd', '10c', 'Kd', 'Qc', 'Ah', '10d'] 
     print('Cards Player2: ', second_team.lst_players[0].cards)
 
     print('announcements:', announcements(sort(second_team.lst_players[0].cards), call))
     announcements_second_player = announcements(sort(second_team.lst_players[0].cards), call)
    
-    second_team.lst_players[0].announcements = list(announcements_second_player.keys())
-    print(second_team.lst_players[0].announcements)
-
     #Player3(first team)
-    first_team.lst_players[1].cards =random_hand(card_deck) 
+    # first_team.lst_players[1].cards =random_hand(card_deck) 
+    first_team.lst_players[1].cards = ['10h', '7c', '8s', '10s', 'Qh', 'As', '8c', 'Jh']
     print('Cards Player3: ', first_team.lst_players[1].cards)
 
     print('announcements:', announcements(sort(first_team.lst_players[1].cards), call))
     announcements_third_player = announcements(sort(first_team.lst_players[1].cards), call)
     
-    first_team.lst_players[1].announcements = list(announcements_third_player.keys())
-    print(first_team.lst_players[1].announcements)
-
     #Player4(second team)
-    second_team.lst_players[1].cards =random_hand(card_deck)
+    # second_team.lst_players[1].cards =random_hand(card_deck)
+    second_team.lst_players[1].cards = ['Qs', '7s', '7d', '8h', 'Js', 'Ks', '9s', '9h']
     print('Cards Player4: ', second_team.lst_players[1].cards)
 
     print('announcements:', announcements(sort(second_team.lst_players[1].cards), call))
     announcements_fourth_player = announcements(sort(second_team.lst_players[1].cards), call)
 
-    second_team.lst_players[1].announcements = list(announcements_fourth_player.keys())
-    print(second_team.lst_players[1].announcements)
-
     announcements_of_the_first_team = team_announcements(announcements_first_player, announcements_third_player)
     announcements_of_the_second_team = team_announcements(announcements_second_player, announcements_fourth_player)
 
-    print('announcements of the first team: ', announcements_of_the_first_team)
-    print('announcements of the second team: ', announcements_of_the_second_team)
+    new_announcements_first_team = compare_team_announcements(announcements_of_the_first_team, announcements_of_the_second_team)[0]
+    new_announcements_second_team = compare_team_announcements(announcements_of_the_first_team, announcements_of_the_second_team)[1]
 
-    print(compare_team_announcements(announcements_of_the_first_team, announcements_of_the_second_team))
+    print('final announcementsTeam1: ', new_announcements_first_team)
+    print('final announcementsTeam2: ', new_announcements_second_team)
+
+    first_team.lst_players[0].announcements = final_announcements(announcements_first_player, new_announcements_first_team)
+    first_team.lst_players[1].announcements = final_announcements(announcements_third_player, new_announcements_first_team)
+
+    second_team.lst_players[0].announcements = final_announcements(announcements_second_player, new_announcements_second_team)
+    second_team.lst_players[1].announcements = final_announcements(announcements_fourth_player, new_announcements_second_team)
+
+    print('final announcementsPlayer1: ', first_team.lst_players[0].announcements)
+    print('final announcementsPlayer2: ', second_team.lst_players[0].announcements)
+    print('final announcementsPlayer3: ', first_team.lst_players[1].announcements)
+    print('final announcementsPlayer4: ', second_team.lst_players[1].announcements)
+
+
+
+
+
 
 
 def main():
--- test_round.py
+++ test_round.py
@@ -1,79 +1,109 @@
 import unittest
-from round import check_team_announcements
+from round import compare_team_announcements, final_announcements
 
-class TestCheckTeamAnnouncements(unittest.TestCase):
+class TestcompareTeamAnnouncements(unittest.TestCase):
     def test_with_two_empty_announcements_returns_list_with_two_empty_dictionaries(self):
-        res = check_team_announcements({}, {})
+        res = compare_team_announcements({}, {})
 
         self.assertEqual(res, [{}, {}])
 
     def test_with_one_empty_announcement_returns_list_with_empty_dictionarie_of_that_empty_team(self):
-        res = check_team_announcements({'belote': [['12s', '13s']]}, {})
-        res2 = check_team_announcements({},{'belote': [['12s', '13s']]})
+        res = compare_team_announcements({'belote': [['12s', '13s']]}, {})
+        res2 = compare_team_announcements({},{'belote': [['12s', '13s']]})
 
         self.assertEqual(res, [{'belote': [['12s', '13s']]}, {}])
         self.assertEqual(res2,[{}, {'belote': [['12s', '13s']]}])
 
     def test_with_tierce_from_one_team_and_quinte_or_quatre_in_the_other_team(self):
-        res = check_team_announcements({'tierce': [['7h', '8h', '9h']]}, {'quinte': [['7s', '8s','9s','10s', '11s']]})
+        res = compare_team_announcements({'tierce': [['7h', '8h', '9h']]}, {'quinte': [['7s', '8s','9s','10s', '11s']]})
 
         self.assertEqual(res, [{'tierce': []}, {'quinte': [['7s', '8s','9s','10s', '11s']]}] )
 
     def test_with_tierces_from_one_team_and_quinte_or_quatre_in_the_other_team(self):
-        res = check_team_announcements({'tierce': [['7h', '8h', '9h'],['7h', '8h', '9h']]}, {'quinte': [['7s', '8s','9s','10s', '11s']]})
+        res = compare_team_announcements({'tierce': [['7h', '8h', '9h'],['7h', '8h', '9h']]}, {'quinte': [['7s', '8s','9s','10s', '11s']]})
 
         self.assertEqual(res, [{'tierce': []}, {'quinte': [['7s', '8s','9s','10s', '11s']]}] )
 
     def test_with_quatre_from_one_team_and_quinte_in_the_other_team(self):
-        res = check_team_announcements({'quatre': [['7h', '8h', '9h','10h']]}, {'quinte': [['7s', '8s','9s','10s', '11s']]})
+        res = compare_team_announcements({'quatre': [['7h', '8h', '9h','10h']]}, {'quinte': [['7s', '8s','9s','10s', '11s']]})
 
         self.assertEqual(res, [{'quatre': []}, {'quinte': [['7s', '8s','9s','10s', '11s']]}] )
 
     def test_with_tierces_from_one_team_and_tierce_in_the_other_team(self):
-        res = check_team_announcements({'tierce': [['7h', '8h', '9h']]}, {'tierce': [['9d', '10d', '11d'], ['7s', '8s','9s']]})
-        res2 = check_team_announcements( {'tierce': [['9d', '10d', '11d'], ['7s', '8s','9s']]}, {'tierce': [['7h', '8h', '9h']]})
+        res = compare_team_announcements({'tierce': [['7h', '8h', '9h']]}, {'tierce': [['9d', '10d', '11d'], ['7s', '8s','9s']]})
+        res2 = compare_team_announcements( {'tierce': [['9d', '10d', '11d'], ['7s', '8s','9s']]}, {'tierce': [['7h', '8h', '9h']]})
 
         self.assertEqual(res, [{'tierce': []}, {'tierce': [['9d', '10d', '11d'], ['7s', '8s','9s']]}] )
         self.assertEqual(res2, [{'tierce': [['9d', '10d', '11d'], ['7s', '8s','9s']]}, {'tierce': []}])
 
     def test_with_tierces_from_one_team_and_tierces_in_the_other_team_which_are_equal(self):
-        res = check_team_announcements({'tierce': [['7h', '8h', '9h']]}, {'tierce': [['7s', '8s','9s']]})
-        res2 = check_team_announcements( {'tierce': [['9d', '10d', '11d'], ['7s', '8s','9s']]}, {'tierce': [['7h', '8h', '9h'],['9c', '10c', '11c']]})
+        res = compare_team_announcements({'tierce': [['7h', '8h', '9h']]}, {'tierce': [['7s', '8s','9s']]})
+        res2 = compare_team_announcements( {'tierce': [['9d', '10d', '11d'], ['7s', '8s','9s']]}, {'tierce': [['7h', '8h', '9h'],['9c', '10c', '11c']]})
 
         self.assertEqual(res, [{'tierce': []}, {'tierce': []}])
         self.assertEqual(res2, [{'tierce': []}, {'tierce': []}])
 
     def test_with_quatre_from_one_team_and_quatre_in_the_other_team_which_are_not_equal(self):
-        res = check_team_announcements({'quatre': [['7h', '8h', '9h', '10h']]}, {'quatre': [['8d','9d', '10d', '11d'], ['7s', '8s','9s','10s']]})
-        res2 = check_team_announcements( {'quatre': [['8d','9d', '10d', '11d'], ['7s', '8s','9s', '10s']]}, {'quatre': [['7h', '8h', '9h', '10h']]})
+        res = compare_team_announcements({'quatre': [['7h', '8h', '9h', '10h']]}, {'quatre': [['8d','9d', '10d', '11d'], ['7s', '8s','9s','10s']]})
+        res2 = compare_team_announcements( {'quatre': [['8d','9d', '10d', '11d'], ['7s', '8s','9s', '10s']]}, {'quatre': [['7h', '8h', '9h', '10h']]})
 
         self.assertEqual(res, [{'quatre': []}, {'quatre': [['8d','9d', '10d', '11d'], ['7s', '8s','9s','10s']]}] )
         self.assertEqual(res2, [{'quatre': [['8d','9d', '10d', '11d'], ['7s', '8s','9s', '10s']]}, {'quatre': []}])
 
     def test_with_quatres_from_one_team_and_quatres_in_the_other_team_which_are_equal(self):
-        res = check_team_announcements({'quatre': [['7h', '8h', '9h', '10h']]}, {'quatre': [['7s', '8s','9s', '10s']]})
-        res2 = check_team_announcements( {'quatre': [['9d', '10d', '11d','12d'], ['7s', '8s','9s','10s']]}, {'quatre': [['7h', '8h', '9h','10h'],['9c', '10c', '11c','12c']]})
+        res = compare_team_announcements({'quatre': [['7h', '8h', '9h', '10h']]}, {'quatre': [['7s', '8s','9s', '10s']]})
+        res2 = compare_team_announcements( {'quatre': [['9d', '10d', '11d','12d'], ['7s', '8s','9s','10s']]}, {'quatre': [['7h', '8h', '9h','10h'],['9c', '10c', '11c','12c']]})
 
         self.assertEqual(res, [{'quatre': []}, {'quatre': []}])
         self.assertEqual(res2, [{'quatre': []}, {'quatre': []}])
 
     def test_with_quinte_from_one_team_and_quinte_in_the_other_team_which_are_not_equal(self):
-        res = check_team_announcements({'quinte': [['7h', '8h', '9h', '10h','11h']]}, {'quinte': [['8d','9d', '10d', '11d','12d'], ['7s', '8s','9s','10s','11s']]})
-        res2 = check_team_announcements( {'quinte': [['8d','9d', '10d', '11d', '12d'], ['7s', '8s','9s', '10s','11s']]}, {'quinte': [['7h', '8h', '9h', '10h','11h']]})
+        res = compare_team_announcements({'quinte': [['7h', '8h', '9h', '10h','11h']]}, {'quinte': [['8d','9d', '10d', '11d','12d'], ['7s', '8s','9s','10s','11s']]})
+        res2 = compare_team_announcements( {'quinte': [['8d','9d', '10d', '11d', '12d'], ['7s', '8s','9s', '10s','11s']]}, {'quinte': [['7h', '8h', '9h', '10h','11h']]})
 
         self.assertEqual(res, [{'quinte': []}, {'quinte': [['8d','9d', '10d', '11d','12d'], ['7s', '8s','9s','10s','11s']]}] )
         self.assertEqual(res2, [{'quinte': [['8d','9d', '10d', '11d', '12d'], ['7s', '8s','9s', '10s','11s']]}, {'quinte': []}])
 
     def test_with_quintes_from_one_team_and_quintes_in_the_other_team_which_are_equal(self):
-        res = check_team_announcements({'quinte': [['7h', '8h', '9h', '10h','11h']]}, {'quinte': [['7s', '8s','9s', '10s', '11s']]})
-        res2 = check_team_announcements( {'quinte': [['8d','9d', '10d', '11d','12d'], ['7s', '8s','9s','10s','11s']]}, {'quinte': [['7h', '8h', '9h','10h','11h'],['8c','9c', '10c', '11c','12c']]})
+        res = compare_team_announcements({'quinte': [['7h', '8h', '9h', '10h','11h']]}, {'quinte': [['7s', '8s','9s', '10s', '11s']]})
+        res2 = compare_team_announcements( {'quinte': [['8d','9d', '10d', '11d','12d'], ['7s', '8s','9s','10s','11s']]}, {'quinte': [['7h', '8h', '9h','10h','11h'],['8c','9c', '10c', '11c','12c']]})
 
         self.assertEqual(res, [{'quinte': []}, {'quinte': []}])
         self.assertEqual(res2, [{'quinte': []}, {'quinte': []}])
 
     def test_with_announcements_which_dont_kill_the_each_other_returns_list_with_dictionaries(self):
-        res = check_team_announcements({'belote': [['12s', '13s']]}, {'tierce': [['8h', '9h', '10h']]})
+        res = compare_team_announcements({'belote': [['12s', '13s']]}, {'tierce': [['8h', '9h', '10h']]})
         self.assertEqual(res, [{'belote': [['12s', '13s']]}, {'tierce': [['8h', '9h', '10h']]}])
+
+class TestFinalAnnouncements(unittest.TestCase):
+    def test_with_no_announcements_return_empty_list(self):
+        res = final_announcements({}, {})
+
+        self.assertEqual(res, [])
+
+    def test_with_announcements_which_are_not_killed_return_list_with_announcements(self):
+        res = final_announcements({'belote': [['12h', '13h']]}, {'belote': [['12h', '13h'], ['12d', '13d']]})
+        res2 = final_announcements({'belote': [['12d', '13d']]}, {'belote': [['12h', '13h'], ['12d', '13d']]})
+
+        self.assertEqual(res, ['belote'])
+        self.assertEqual(res2, ['belote'])
+
+    def test_with_announcements_which_are_killed_return_list_with_announcements(self):
+        res = final_announcements({'tierce': [['7h', '8h', '9h']]}, {'tierce': []})
+        res2 = final_announcements({'belote': [['12d', '13d']], 'tierce': [['7h', '8h', '9h']]}, {'belote': [['12d', '13d']], 'tierce': []})
+
+        self.assertEqual(res, [])
+        self.assertEqual(res2, ['belote'])
+
+    def test_with_no_announcements_of_the_one_player_empty_list_with_announcements(self):
+        res = final_announcements({}, {'tierce': [['7d', '8d', '9d']]})
+
+        self.assertEqual(res, [])
+
+    def test_with_one_announcements_of_the_one_player_and_one_of_the_other_return_announcements_of_the_player(self):
+        res = final_announcements({'tierce': [['9d', '10d', '11d']]}, {'tierce': [['9d', '10d', '11d'], ['11s', '12s', '13s']], 'belote': [['12s', '13s']]})
+
+        self.assertEqual(res, ['tierce'])
 
 
 if __name__ == '__main__':
